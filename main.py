cac9f7c4bc5fa42b1414a6ad7a1fdd9d59f5d0c8
Norgius <dmitry.schadrin21@yandex.ru>
2022-10-11 11:53:20 +0300

    Formatted code with PEP8 requirements and all function arguments was specified

--- main.py
+++ main.py
@@ -9,21 +9,17 @@ import requests.exceptions
 from dotenv import load_dotenv
 
 logger = logging.getLogger(__file__)
-API_VERSION = 5.131
-SENDER = 0
-INTERVAL = 86400
-FILEPATH = './comics.png'
 
 
-def download_image(url):
+def download_image(url, filepath):
     logger.info('download_image() was called')
     response = requests.get(url)
     response.raise_for_status()
-    with open(FILEPATH, 'wb') as file:
+    with open(filepath, 'wb') as file:
         file.write(response.content)
 
 
-def receive_comic(comics_number=''):
+def receive_comic(filepath, comics_number=''):
     logger.info('receive_comic() was called')
     url = f'https://xkcd.com/{comics_number}/info.0.json'
     response = requests.get(url)
@@ -33,25 +29,28 @@ def receive_comic(comics_number=''):
         return unpacked_response.get('num')
     img_link = unpacked_response.get('img')
     comics_comment = unpacked_response.get('alt')
-    download_image(img_link)
+    download_image(img_link, filepath)
     return comics_comment
 
 
-def get_server_address_for_uploading_photo(access_token, group_id):
+def get_server_address_for_uploading_photo(
+        access_token, api_version, group_id):
     logger.info('get_server_address_for_uploading_photo() was called')
     url = 'https://api.vk.com/method/photos.getWallUploadServer'
-    params = {'access_token': access_token, 'v': API_VERSION,
-              'group_id': group_id
-              }
+    params = {
+        'access_token': access_token,
+        'v': api_version,
+        'group_id': group_id,
+    }
     response = requests.get(url, params=params)
     response.raise_for_status()
     unpacked_response = response.json()
     return unpacked_response.get('response').get('upload_url')
 
 
-def upload_img_to_server(url):
+def upload_img_to_server(filepath, url):
     logger.info('upload_img_to_server() was called')
-    with open(FILEPATH, 'rb') as file:
+    with open(filepath, 'rb') as file:
         files = {
             'photo': file,
         }
@@ -60,11 +59,15 @@ def upload_img_to_server(url):
     return response.json()
 
 
-def save_img_to_group_album(access_token, group_id, server_response):
+def save_img_to_group_album(
+        access_token, api_version,
+        group_id, server_response):
     logger.info('save_img_to_group_album() was called')
-    params = {'access_token': access_token, 'v': API_VERSION,
-              'group_id': group_id,
-              }
+    params = {
+        'access_token': access_token,
+        'v': api_version,
+        'group_id': group_id,
+    }
     params.update(server_response)
     url = 'https://api.vk.com/method/photos.saveWallPhoto'
     response = requests.post(url, params=params)
@@ -75,41 +78,75 @@ def save_img_to_group_album(access_token, group_id, server_response):
     return photo_owner_id, photo_id
 
 
-def post_comic_on_group_wall(access_token, group_id, comics_comment,
-                             photo_owner_id, photo_id):   
+def post_comic_on_group_wall(
+        access_token, api_version,
+        group_id, comics_comment, sender,
+        photo_owner_id, photo_id):
     logger.info('post_comic_on_group_wall() was called')
     url = 'https://api.vk.com/method/wall.post'
-    params = {'access_token': access_token, 'v': API_VERSION,
-              'owner_id': f'-{group_id}', 'from_group': SENDER,
-              'attachments': f'photo{photo_owner_id}_{photo_id}',
-              'message': comics_comment,
-              }
+    params = {
+        'access_token': access_token,
+        'v': api_version,
+        'owner_id': f'-{group_id}',
+        'from_group': sender,
+        'attachments': f'photo{photo_owner_id}_{photo_id}',
+        'message': comics_comment,
+    }
     response = requests.post(url, params=params)
     response.raise_for_status()
 
 
 def main():
-    logging.basicConfig(filename='app.log', filemode='w', level=logging.INFO,
-                        format='%(name)s - %(levelname)s '
-                               '- %(asctime)s - %(message)s')
+    logging.basicConfig(
+        filename='app.log',
+        filemode='w',
+        level=logging.INFO,
+        format='%(name)s - %(levelname)s - %(asctime)s - %(message)s',
+    )
     logger.setLevel(logging.INFO)
-    handler = RotatingFileHandler('app.log', maxBytes=10000, backupCount=2)
+    handler = RotatingFileHandler(
+        filename='app.log',
+        maxBytes=10000,
+        backupCount=2
+    )
     logger.addHandler(handler)
     load_dotenv()
     access_token = os.getenv('ACCESS_TOKEN_TO_COMICS_VK')
     group_id = os.getenv('GROUP_ID_COMICS_VK')
+    filepath = './comics.png'
+    api_version = 5.131
+    sender = 0
     try:
-        comics_amount = receive_comic()
+        comics_amount = receive_comic(filepath=filepath)
         random_number = randint(1, comics_amount)
-        comics_comment = receive_comic(random_number)
+        comics_comment = receive_comic(
+            filepath=filepath,
+            comics_number=random_number,
+        )
         server_address = get_server_address_for_uploading_photo(
-                                          access_token, group_id)
-        server_response = upload_img_to_server(server_address)
-        photo_owner_id, photo_id = save_img_to_group_album(access_token,
-                                                           group_id,
-                                                           server_response)
-        post_comic_on_group_wall(access_token, group_id, comics_comment,
-                                 photo_owner_id, photo_id)
+            access_token=access_token,
+            api_version=api_version,
+            group_id=group_id,
+        )
+        server_response = upload_img_to_server(
+            filepath=filepath,
+            server_address=server_address,
+        )
+        photo_owner_id, photo_id = save_img_to_group_album(
+            access_token=access_token,
+            api_version=api_version,
+            group_id=group_id,
+            server_response=server_response,
+        )
+        post_comic_on_group_wall(
+            access_token=access_token,
+            api_version=api_version,
+            group_id=group_id,
+            comics_comment=comics_comment,
+            sender=sender,
+            photo_owner_id=photo_owner_id,
+            photo_id=photo_id,
+        )
     except requests.exceptions.HTTPError as http_er:
         logger.warning(f'\n{http_er}\n')
         sys.stderr.write(f'\n{http_er}\n')
@@ -117,7 +154,7 @@ def main():
         logger.warning(f'\n{connect_er}\n')
         sys.stderr.write(f'\n{connect_er}\n')
     finally:
-        os.remove(FILEPATH)
+        os.remove(filepath)
 
 
 if __name__ == '__main__':
